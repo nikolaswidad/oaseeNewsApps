b135224677dd68c3c61559e288c64be6e8a8f32b
nikolaswidad <70227834+nikolaswidad@users.noreply.github.com>
2023-06-12 04:24:15 +0700

    add BottomSheetScaffolld
    belum sempurna

--- app/src/main/java/com/nikolaswidad/oasenews/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/nikolaswidad/oasenews/ui/screen/detail/DetailScreen.kt
@@ -7,32 +7,54 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.nikolaswidad.oasenews.R
 import com.nikolaswidad.oasenews.datasource.local.entity.NewsEntity
 import com.nikolaswidad.oasenews.ui.components.TopBar
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DetailScreen(
     news: NewsEntity,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val sheetState = rememberBottomSheetState(
+        initialValue = BottomSheetValue.Collapsed)
+
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = sheetState
+    )
+    val scope = rememberCoroutineScope()
 
     Column(modifier = modifier) {
         TopBar(
@@ -41,7 +63,46 @@ fun DetailScreen(
             isBackVisible = true,
             isBookmarkVisible = true,
             onBackClick = { navigateBack() })
-        DetailContent(news)
+
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+
+            sheetContent = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Bottom sheet",
+                        fontSize = 60.sp
+                    )
+                }
+            },
+            sheetBackgroundColor = Color.Green
+        ) {
+
+            FloatingActionButton(onClick = {
+                scope.launch {
+                    if (sheetState.isCollapsed) {
+                        sheetState.expand()
+                    } else {
+                        sheetState.collapse()
+                    }
+                }
+            }) {
+                Text(text = "Toggle Sheet")
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                DetailContent(news)
+            }
+        }
     }
 }
 
@@ -66,3 +127,4 @@ fun DetailContent(
         })
     }
 }
+
